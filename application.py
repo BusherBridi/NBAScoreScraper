d133a5041bb3cba55e2f2cee7de069880f408215
Busher Bridi <bridibusher@gmail.com>
2020-08-07 15:15:57 -0700

    Added soupify function that returns soup of a webapage

--- application.py
+++ application.py
@@ -5,16 +5,24 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as ureq
 
 application = app = Flask(__name__)
-
-@app.route("/<string:team>", methods = ["GET"])
-def getScores(team):
-    url = "https://www.cbssports.com/nba/gametracker/boxscore/NBA_20200806_" + team + '/'
+# Function to soupify a page
+def soupify(url):
     uclient = ureq(url)
     page_html = uclient.read()
     uclient.close()
     page_soup = soup(page_html, "html.parser")
+    return page_soup
+
+@app.route("/<string:team>", methods = ["GET"])
+def getScores(team):
+    url = "https://www.cbssports.com/nba/gametracker/boxscore/NBA_20200806_" + team + '/'
+    page_soup = soupify(url)
     scores = page_soup.findAll("div",{"class":"score-text"})
     home_score = scores[0].text
     away_score = scores[1].text
     return jsonify({"home_score":home_score,"away_score":away_score})
-    
+
+@app.route("/schedule", methods = ["GET"])
+def getSchedule():
+    url = "https://www.msn.com/en-us/Sports/nba/schedule"
+    page_soup = soupify(url)
