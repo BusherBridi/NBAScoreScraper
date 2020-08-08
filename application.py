3783e5206eac7fe8378c4f8d409c24c4cce0b01a
Busher Bridi <bridibusher@gmail.com>
2020-08-07 19:30:54 -0700

    fixed jets dumbass mistakes

--- application.py
+++ application.py
@@ -9,6 +9,11 @@ application = app = Flask(__name__)
 
 # Function to soupify a page
 def soupify(url):
+    uclient = ureq(url)
+    page_html = uclient.read()
+    uclient.close()
+    page_soup = soup(page_html, "html.parser")
+    return page_soup
 @app.route("/")
 def index():
     return ("NBA_SCRAPER")
@@ -18,22 +23,12 @@ def getScores(team):
     today = date.today() # Get today's date
     url_formated_date = today.strftime("%Y%m%d")
     url = "https://www.cbssports.com/nba/gametracker/boxscore/NBA_"+url_formated_date+"_" + team + '/'
-
-    uclient = ureq(url)
-    page_html = uclient.read()
-    uclient.close()
-    page_soup = soup(page_html, "html.parser")
-    return page_soup
-
-
-@app.route("/score/<string:team>", methods = ["GET"])
-def getScores(team):
-    url = "https://www.cbssports.com/nba/gametracker/boxscore/NBA_20200806_" + team + '/'
     page_soup = soupify(url)
     scores = page_soup.findAll("div",{"class":"score-text"})
     home_score = scores[0].text
     away_score = scores[1].text
     return jsonify({"home_score":home_score,"away_score":away_score})
+
 
 # @app.route("/schedule", methods = ["GET"])
 # def getSchedule():
