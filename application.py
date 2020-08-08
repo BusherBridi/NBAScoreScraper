1c08a1ce8224f8a4fdd1cc2d2d50494ba90df2c8
Busher Bridi <bridibusher@gmail.com>
2020-08-07 19:23:22 -0700

    Commented out unreachable code

--- application.py
+++ application.py
@@ -35,20 +35,20 @@ def getScores(team):
     away_score = scores[1].text
     return jsonify({"home_score":home_score,"away_score":away_score})
 
-@app.route("/schedule", methods = ["GET"])
-def getSchedule():
-    url = "https://www.msn.com/en-us/Sports/nba/schedule"
-    liveGame = "No Live Games"
-    try:
-        page_soup = soupify(url)
-        liveGame = page_soup.find("div",{"id":"live"}).findAll("td",{"class":"teamname"})
-    except:
-        return jsonify({"games":liveGame}), 200
-    numberOfLiveGames = len(liveGame) / 4
-    it = iter(liveGame)
-    onGoingGames = {}
-    gameId = 0
-    for x in it:
-        onGoingGames = {""}
-   # return str(liveGame[0].div.text)
-    return str(liveGame)
+# @app.route("/schedule", methods = ["GET"])
+# def getSchedule():
+#     url = "https://www.msn.com/en-us/Sports/nba/schedule"
+#     liveGame = "No Live Games"
+#     try:
+#         page_soup = soupify(url)
+#         liveGame = page_soup.find("div",{"id":"live"}).findAll("td",{"class":"teamname"})
+#     except:
+#         return jsonify({"games":liveGame}), 200
+#     numberOfLiveGames = len(liveGame) / 4
+#     it = iter(liveGame)
+#     onGoingGames = {}
+#     gameId = 0
+#     for x in it:
+#         onGoingGames = {""}
+#    # return str(liveGame[0].div.text)
+#     return str(liveGame)
